dffb6c9c6e4c696c2d2da86d74ff93b71e4c3f08
Mo Chen <uncorrupt@gmail.com>
2022-01-18 17:54:33 -0600

    Improve guess generation
    Look at all possible words, rather than just the ones which are possible correct words.

--- wordle-solve.py
+++ wordle-solve.py
@@ -15,11 +15,12 @@ def main():
     solve(positionals, haves, nots)
 
 def solve(known_positions: list, known_letters: set, not_letters: set):
-    possible_words = set()
+    all_words = set()
     with open('words-5.txt') as f:
         for word in f.readlines():
-            possible_words.add(word.strip())
+            all_words.add(word.strip())
 
+    possible_words = all_words.copy()
     for i, l in enumerate(known_positions):
         if l is not None:
             possible_words = set(pw for pw in possible_words if pw[i] == l)
@@ -30,9 +31,13 @@ def solve(known_positions: list, known_letters: set, not_letters: set):
     for l in not_letters:
         possible_words = set(pw for pw in possible_words if l not in pw)
     
-    print(f'{len(possible_words)} possible words')
+    
     
     possible_count = len(possible_words)
+    print(f'{possible_count} possible words')
+    if possible_count < 100:
+        print(possible_words)
+
     best_letters = {}
     for l in ascii_lowercase:
         have_count = sum(1 for pw in possible_words if l in pw)
@@ -40,16 +45,17 @@ def solve(known_positions: list, known_letters: set, not_letters: set):
         discriminating_power = min(have_count, not_count)
         best_letters[l] = discriminating_power
     
-    print(best_letters)
+    # print(best_letters)
 
     best_words = {}
-    for pw in possible_words:
+    for pw in all_words:
         score = sum(best_letters[l] for l in set(pw))
         best_words[pw] = score
     
     #print(best_words)
-    best_words_list = list(sorted(best_words.items(), key=lambda item: item[1]))
-    print(best_words_list)
+    best_words_list = list(sorted(best_words.items(), key=lambda item: item[1], reverse=True))
+    for bw in best_words_list[:10]:
+        print(f'guess: {bw[0]}, score: {bw[1]}')
 
 if __name__ == '__main__':
     main()
